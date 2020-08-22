f41b123293cfad993b1c627e9d9df528674a2786
Shivangi Upadhyay <shivangi.upadhyay@nagarro.com>
2020-08-22 22:25:09 +0530

    recurrsion

--- recurssion.js
+++ recurssion.js
@@ -29,3 +29,16 @@ function productOfArray(arr){
     else
     return arr.pop() * productOfArray(arr);
 }
+
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55 
+
+function recursiveRange(num){
+    if(num === 0)
+      return 0;
+   else 
+     return num + recursiveRange(num-1);
+  }
+  
+  
