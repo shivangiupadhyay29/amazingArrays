87ddbc8417ac0d05ac9a99023bd13e4c1c8d045f
Shivangi Upadhyay <shivangi.upadhyay@nagarro.com>
2020-08-22 21:06:05 +0530

    recurrsion

--- recurssion.js
+++ recurssion.js
@@ -8,3 +8,15 @@ function power(base,exp){
     else 
     return base*power(base,exp-1);
 }
+
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
+
+function factorial(num){
+    if(num === 0)
+    return 1;
+    else 
+    return num*factorial(num-1);
+ }
