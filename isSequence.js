dedcba979fa1a16e0ba583678e0a9fe84e704026
Shivangi Upadhyay <shivangi.upadhyay@nagarro.com>
2020-08-22 10:42:25 +0530

    sequence check.

---
+++ isSequence.js
@@ -0,0 +1,16 @@
+function isSubsequence(str1, str2) {
+    var i = 0;
+    var j = 0;
+    if (!str1) return true;
+    while (j < str2.length) {
+      if (str2[j] === str1[i]) i++;
+      if (i === str1.length) return true;
+      j++;
+    }
+    return false;
+  }
+  
+  console.log(isSubsequence('hello', 'hello world'));
+  console.log(isSubsequence('sing', 'sting'));
+  console.log(isSubsequence('abc', 'acb'));
+  console.log(isSubsequence('abc', 'abracadabra'));
