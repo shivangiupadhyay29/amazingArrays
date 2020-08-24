d5eb130751fc051610ee09fac32e5921e1d86be4
shivangiupadhyay29 <shivangi.upadhyay@nagarro.com>
2020-08-24 08:53:52 +0530

    flat array fix.

--- flattenArray.js
+++ flattenArray.js
@@ -13,4 +13,4 @@ function flatten(arr){
   console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
   console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
   console.log(flatten([[1],[2],[3]])) // [1,2,3]
-  console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3
+  console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
